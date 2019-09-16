ac814aaaadf592a3500e08a831111d61644b5d44
kaiyun213 <996741645@qq.com>
2019-09-16 17:13:48 +0800

    登录页面布局完成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
+
+//导入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+//使用 vant
+Vue.use(Vant);
+
+
+
 
 new Vue({
   router,
---
+++ src/views/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- 顶部登录条 -->
+    <van-nav-bar title="登录" />
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field v-model="mobile" required clearable label="手机号码" placeholder="请输入手机号码" />
+      <van-field v-model="code" required label="验证码" placeholder="请输入验证码" />
+    </van-cell-group>
+    <!-- 底部登录按钮 -->
+    <div class="login-btn">
+      <van-button type="info" size="large">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: '',
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.van-nav-bar {
+  background-color: #1989fa;
+  .van-nav-bar__title {
+    color: #fff;
+  }
+}
+.login-btn {
+  margin: 30px 20px;
+  .van-button {
+    border-radius: 5px;
+  }
+}
+</style>
